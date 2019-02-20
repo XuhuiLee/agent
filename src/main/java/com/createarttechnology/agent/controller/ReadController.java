c04c5aae0ea570222682ee3dc07a06a87c4bde5b
XuhuiLee <lxh199108@gmail.com>
2019-02-20 18:16:56 +0800

    test接口

--- src/main/java/com/createarttechnology/agent/controller/ReadController.java
+++ src/main/java/com/createarttechnology/agent/controller/ReadController.java
@@ -4,12 +4,14 @@ import com.createarttechnology.logger.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.InputStreamReader;
 
 /**
  * Created by lixuhui on 2019/1/28.
@@ -20,7 +22,8 @@ public class ReadController {
     private static final Logger logger = Logger.getLogger(ReadController.class);
 
     @RequestMapping("/cmd/{cmd}")
-    public void index(HttpServletRequest request, HttpServletResponse response, @PathVariable String cmd) throws IOException {
+    @ResponseBody
+    public String index(HttpServletRequest request, HttpServletResponse response, @PathVariable String cmd) throws IOException {
 
         Process process = Runtime.getRuntime().exec(cmd);
         try {
@@ -29,21 +32,21 @@ public class ReadController {
             e.printStackTrace();
         }
         int exitVal = process.exitValue();
-        InputStream is = null;
-        OutputStream os = response.getOutputStream();
+        InputStream is;
         if (exitVal == 0) {
             is = process.getInputStream();
         } else {
             is = process.getErrorStream();
         }
-        response.setStatus(200);
-        byte[] buff = new byte[1024];
-        while (is.read(buff) >= 0) {
-            os.write(buff);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line);
         }
-        os.flush();
+        reader.close();
         is.close();
-        os.close();
+        return sb.toString();
     }
 
 }
